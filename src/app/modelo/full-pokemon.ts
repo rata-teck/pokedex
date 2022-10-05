e682d0918d2972e1a903f1e6af888e5979c63c7d
rata-teck <shadowsmash5@gmail.com>
2022-10-05 17:16:46 -0300

    full2

--- src/app/modelo/full-pokemon.ts
+++ src/app/modelo/full-pokemon.ts
@@ -15,4 +15,42 @@ export interface FullPokemon {
         }
     ],
     height : number,
+    held_items : [
+      item : {
+        name : string,
+        url : string
+      },
+      version_details : [
+        {
+          rarity : number,
+          version : {
+            name : string,
+            url : string
+          }
+        }
+      ]
+    ],
+    id : number,
+    is_default : boolean,
+    location_area_encounters : string,
+    moves : [
+      {
+      move : {
+        name : string;
+        url : string;
+      },
+      version_group_details : [{
+        level_learned_at : number,
+        move_learn_method : {
+          name : string,
+          url : string
+        },
+        version_group : {
+          name : string,
+          url : string
+        }
+      }
+    ]
+  }
+  ]
 }
